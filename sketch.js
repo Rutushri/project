ea48ef08d876725a7ca111696768aa91f304b3e6
Rutushri <113025752+Rutushri@users.noreply.github.com>
2022-09-14 03:51:40 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+var friends = ["ram","sam","amay"]
+
+console.log(friends[0])
+console.log(friends[1]) 
+console.log(friends[2])
+console.log(friends.length)
